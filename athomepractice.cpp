41e384a5ad7702cf6e0258eb8021a2b759854b40
xavier.diaz11@upr.edu <xavier.diaz11@upr.edu>
2025-02-07 03:13:22 -0800

    changed. cpp file again

--- athomepractice.cpp
+++ athomepractice.cpp
@@ -1,7 +1,13 @@
 # include <iostream>
 using namespace std;
 int main(){
-    char A;
-    cin>> A;
-    cout<<A;
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    string text;
+    cout << "Enter some text: ";
+    getline(cin, text);
+
+    cout << "You entered: " << num << " and \"" << text << "\"" << endl;
+    return 0;
 }
